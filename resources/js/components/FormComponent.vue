6d4ac4954dd746420fbc38fbd7a7ac164e630108
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-16 13:19:07 +0800

    Creating Auth and Modified Home/Dashboard

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-md-4">
+        <form action="">
+            <div class="form-group">
+                <textarea name=""
+                          id=""
+                          cols="8"
+                          rows="8"
+                          maxlength="130"
+                class="form-control"
+                required>
+                </textarea>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary">
+                    Amping
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-4">
+            Tweet Form
+        </div>
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            Timeline
         </div>
     </div>
 </div>
